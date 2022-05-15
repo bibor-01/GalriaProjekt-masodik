1afd34f4f2b733777ddb11f853eeaa48966454f1
bibor-01 <balogh01biborka@gmail.com>
2022-05-15 17:16:41 +0200

    main-ben galéria példányosítása, cím, készítő, megjelenít metódus kiírása foreachel való bejárással, kep.txt létrehozása

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,10 +1,29 @@
-
 package main;
+
+import modell.Festmeny;
+import modell.Galeria;
+import modell.KiallitasiTargy;
 
 public class Program {
 
     public static void main(String[] args) {
-       
+        Galeria galeria = new Galeria();
+
+        System.out.println("cím szerinti rendezést");
+        for (KiallitasiTargy kiallitasiTargy : galeria) {
+            System.out.println(kiallitasiTargy);
+        }
+
+        System.out.println("készítő szerinti rendezést");
+        for (KiallitasiTargy kiallitasiTargy : galeria) {
+            System.out.println(kiallitasiTargy);
+        }
+
+        for (KiallitasiTargy targy : galeria) {
+            if (targy instanceof Festmeny) {
+                ((Festmeny) targy).megjelenit();
+            }
+        }
     }
-    
+
 }
--- src/modell/Galeria.java
+++ src/modell/Galeria.java
@@ -1,15 +1,44 @@
 package modell;
 
+import java.io.File;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Galeria implements Iterable<KiallitasiTargy>{
+public class Galeria implements Iterable<KiallitasiTargy> {
 
     private ArrayList<KiallitasiTargy> targyak;
 
     public Galeria() {
+
+        try {
+            //készítő és cím
+            Festmeny f1 = new Festmeny("keszito1", "cim3");
+            //készítő és cím a mai dátummal
+            Festmeny f2 = new Festmeny(LocalDate.now(), "keszito5", "cim1");
+            //készítő és cím a hibás dátummal
+            Festmeny f3 = new Festmeny(LocalDate.parse("2025-02-11"), "keszito2", "cim4");
+            //készítő és cím fájl eléréssel
+            Festmeny f4 = new Festmeny(new File("kep.txt"), "keszito3", "cim2");
+            //készítő és cím fájl eléréssel, mai dátummal
+            Festmeny f5 = new Festmeny(new File("kep.txt"), LocalDate.now(), "keszito4", "cim5");
+            targyak.add(f1);
+            targyak.add(f2);
+            targyak.add(f3);
+            targyak.add(f4);
+            targyak.add(f5);
+
+        } catch (HibasDatumException ex) {
+            Logger.getLogger(Galeria.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void mentes(KiallitasiTargy targy){
+        targyak.add(targy);
     }
 
     public List<KiallitasiTargy> rendezCimSzerint() {
@@ -24,6 +53,6 @@ public class Galeria implements Iterable<KiallitasiTargy>{
 
     @Override
     public Iterator<KiallitasiTargy> iterator() {
-       return Collections.unmodifiableList(targyak).iterator();
+        return Collections.unmodifiableList(targyak).iterator();
     }
 }
