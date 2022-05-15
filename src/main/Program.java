5ce769784397a44936aeba1cb5e36975f692e897
bibor-01 <balogh01biborka@gmail.com>
2022-05-15 17:21:48 +0200

    kiírás és visszaolvasás

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import modell.Festmeny;
 import modell.Galeria;
 import modell.KiallitasiTargy;
@@ -24,6 +29,36 @@ public class Program {
                 ((Festmeny) targy).megjelenit();
             }
         }
-    }
 
+        //kiírás:
+        try {
+            FileOutputStream fileOut = new FileOutputStream("galeria.bin");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(galeria);
+            out.close();
+            fileOut.close();
+            System.out.printf("sikeres kiírás!");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        galeria = null;
+
+        //Visszaolvasás
+        try {
+            ObjectInputStream objBe = new ObjectInputStream(new FileInputStream("galeria.bin"));
+            galeria = (Galeria) objBe.readObject();
+            objBe.close();
+            System.out.println("Visszaállítás:");
+            for (KiallitasiTargy targy : galeria) {
+                System.out.println(targy);
+            }
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Sikertelen beolvasás!");
+            c.printStackTrace();
+            return;
+        }
+    }
 }
--- src/modell/Galeria.java
+++ src/modell/Galeria.java
@@ -1,6 +1,7 @@
 package modell;
 
 import java.io.File;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -9,26 +10,27 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Galeria implements Iterable<KiallitasiTargy> {
+public class Galeria implements Iterable<KiallitasiTargy>, Serializable {
 
     private ArrayList<KiallitasiTargy> targyak;
 
     public Galeria() {
 
+        targyak = new ArrayList<>();
         try {
             //készítő és cím
             Festmeny f1 = new Festmeny("keszito1", "cim3");
             //készítő és cím a mai dátummal
             Festmeny f2 = new Festmeny(LocalDate.now(), "keszito5", "cim1");
             //készítő és cím a hibás dátummal
-            Festmeny f3 = new Festmeny(LocalDate.parse("2025-02-11"), "keszito2", "cim4");
+            //Festmeny f3 = new Festmeny(LocalDate.parse("2025-02-11"), "keszito2", "cim4");
             //készítő és cím fájl eléréssel
             Festmeny f4 = new Festmeny(new File("kep.txt"), "keszito3", "cim2");
             //készítő és cím fájl eléréssel, mai dátummal
             Festmeny f5 = new Festmeny(new File("kep.txt"), LocalDate.now(), "keszito4", "cim5");
             targyak.add(f1);
             targyak.add(f2);
-            targyak.add(f3);
+            //targyak.add(f3);
             targyak.add(f4);
             targyak.add(f5);
 
@@ -36,8 +38,8 @@ public class Galeria implements Iterable<KiallitasiTargy> {
             Logger.getLogger(Galeria.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void mentes(KiallitasiTargy targy){
+
+    public void mentes(KiallitasiTargy targy) {
         targyak.add(targy);
     }
 
